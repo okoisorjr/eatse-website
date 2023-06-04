0825ac1ce5c2ebbf86e253d5fb0646873d3a53d1
okoisorjr <okoisorjr@gmail.com>
2023-06-05 00:34:08 +0100

    fix to the social media links and logout redirect for mobile as well as fix for laundry service booking

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,38 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
-import { CurrentUser } from 'src/app/auth/auth.service';
 import { GlobalResourceService } from 'src/app/global-resource/global-resource.service';
 import { ContactEatseService } from 'src/app/services/contact-eatse.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
-
   fullname!: string;
   email!: string;
   phone!: string;
   message!: string;
   currentUser: any;
 
-  constructor(private auth: Auth, private contactService: ContactEatseService) { 
+  constructor(private auth: Auth, private contactService: ContactEatseService) {
     this.auth.onAuthStateChanged((credential) => {
-      if(credential){
+      if (credential) {
         this.currentUser = credential;
+        this.email = this.currentUser.email;
+        this.fullname = this.currentUser.displayName;
       }
-    })
+    });
   }
 
-  ngOnInit(): void {
-    this.email = this.currentUser.email;
-    this.fullname = this.currentUser.displayName;
-  }
+  ngOnInit(): void {}
 
-  submitForm(form: any){
+  submitForm(form: any) {
     console.log(form.value);
     this.contactService.sendMessage(form.value);
   }
-
 }
