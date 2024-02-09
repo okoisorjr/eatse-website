7e9c60659f03d60db1be70897bf286720220b14a
okoisorjr <okoisorjr@gmail.com>
2024-02-10 00:09:15 +0100

    fridays changes

--- src/app/pages/careers/job-details/job-details.component.ts
+++ src/app/pages/careers/job-details/job-details.component.ts
@@ -1,5 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { CareerService } from 'src/app/services/career.service';
 
 @Component({
   selector: 'app-job-details',
@@ -8,31 +10,52 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class JobDetailsComponent implements OnInit {
   roles: any[] = [];
-  activeRole!: string;
+  activeRole: any;
+  all_openings: any[] = [];
+  role_id!: string;
 
-  constructor(private ar: ActivatedRoute, private router: Router) {}
+  constructor(
+    private ar: ActivatedRoute,
+    private router: Router,
+    private careerService: CareerService
+  ) {
+    this.role_id = this.ar.snapshot.params['role_id'];
+  }
+
+  fetchAllOpenings() {
+    this.careerService.fetchAllOpenings().subscribe(
+      (value) => {
+        this.all_openings = value;
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    );
+  }
 
   ngOnInit(): void {
-    this.roles = [
-      { role: 'Easer', link: 'easer' },
-      { role: 'Business Developer', link: 'business-developer' },
-      { role: 'Frontend Developer', link: 'frontend-developer' },
-      { role: 'Backend Developer', link: 'backend-developer' },
-      { role: 'Product Designer', link: 'product-designer' },
-    ];
+    this.careerService.fetchSingleOpenPosition(this.role_id).subscribe(
+      (value) => {
+        this.activeRole = value;
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    );
 
     this.roles.find((role) => {
       if (role.link == this.ar.snapshot.params['id']) {
         this.activeRole = role.role;
       }
     });
+    this.fetchAllOpenings();
   }
 
-  updateActiveRole(role: string) {
+  updateActiveRole(role: any) {
     this.activeRole = role;
   }
 
   apply() {
-    this.router.navigate([`/careers/role/${this.activeRole}/apply`]);
+    this.router.navigate([`/careers/role/${this.activeRole._id}/apply`]);
   }
 }
